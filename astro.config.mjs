6b47ac61786d366feb756b4ae05ad6622deb805b
Henry Percy <henry@rework.digital>
2023-10-09 19:22:00 +0100

    feat: install and setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 import vue from "@astrojs/vue";
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [vue()]
+  integrations: [vue(), tailwind()]
 });
